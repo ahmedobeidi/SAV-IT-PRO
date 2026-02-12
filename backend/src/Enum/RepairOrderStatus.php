1137073eca4763a43cc745616c2f9f07b5d06bc7
Ahmed AL-OBEIDI <alobeidi.dev@gmail.com>
2026-02-12 17:30:30 +0100

    MODIFY: RepairOrderStatus Enum

--- backend/src/Enum/RepairOrderStatus.php
+++ backend/src/Enum/RepairOrderStatus.php
@@ -2,11 +2,12 @@
 
 namespace App\Enum;
 
-enum RepairOrderStatus: string
-{
-    case NEW = 'NEW';
+enum RepairOrderStatus: string {
+    case CREATED = 'CREATED';
+    case ASSIGNED = 'ASSIGNED';
     case IN_PROGRESS = 'IN_PROGRESS';
     case WAITING_PARTS = 'WAITING_PARTS';
     case DONE = 'DONE';
-    case CANCELLED = 'CANCELLED';
+    case DELIVERED = 'DELIVERED';
+    case CANCELED = 'CANCELED';
 }
