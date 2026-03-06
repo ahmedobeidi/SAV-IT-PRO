fa7154052108fa356ebe2f93b9664bf494fac93b
Ahmed AL-OBEIDI <alobeidi.dev@gmail.com>
2026-03-06 21:26:18 +0100

    ADD: logo

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,15 +1,24 @@
 import { Outlet } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 export default function AuthLayout() {
   return (
     <div
       style={{
         minHeight: "100%",
-        display: "grid",
-        placeItems: "center",
+        display: "flex",
+        flexDirection: 'column',
+        justifyContent: "center",
+        alignItems: "center",
         padding: 24,
       }}
     >
+      <img
+        src={logo}
+        alt="Logo"
+        style={{ width: 120, marginBottom: 16, borderRadius: 30, cursor: "pointer" }}
+      />
+
       <div className="card" style={{ width: "100%", maxWidth: 420, padding: 20 }}>
         <Outlet />
       </div>
